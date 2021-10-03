146b4534b50035e24c8704b0b55496354a85daee
21grahams <graham.kirsh@gmail.com>
2021-10-02 18:25:16 -0600

    added navbar with links to about and home. added layout component which wraps app.js inside of styles

---
+++ my-app/pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <h3>This is the About Section</h3>
+    </div>
+  );
+};
+
+export default About;
